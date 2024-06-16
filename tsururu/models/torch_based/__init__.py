95fe4be75c26a019bc25e94fa14d4cee4735e462
Alina_Kostromina <alina.kostromina@gmail.com>
2024-06-17 00:03:04 +0300

    example demand

--- tsururu/models/torch_based/__init__.py
+++ tsururu/models/torch_based/__init__.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 """NN algorithms for time series forecasting."""
 
 from .dlinear import DLinear_NN
 
 __all__ = ["Dlinear_NN"]
-=======
-"""NN algorithms for time series forecasting."""
-
-from .dlinear import DLinear_NN
-
-__all__ = ["Dlinear_NN"]
->>>>>>> origin/feature/new_models
