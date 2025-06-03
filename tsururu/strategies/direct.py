b73787b61de86aa08b2adf5aaf02f486e0837ec6
Aleksandr Yugay <sasha.yugay1999@mail.ru>
2025-06-03 17:26:20 +0300

    recursive predictions for arbitrary horizon

--- tsururu/strategies/direct.py
+++ tsururu/strategies/direct.py
@@ -68,6 +68,9 @@ class DirectStrategy(RecursiveStrategy):
         """
         self.trainers = []
 
+        # intrinsic_horizon is a multiple of model_horizon
+        intrinsic_horizon = self.model_horizon * ((self.horizon + self.model_horizon - 1) // self.model_horizon)
+
         if self.equal_train_size:
             features_idx = index_slicer.create_idx_data(
                 dataset.data,
@@ -118,10 +121,10 @@ class DirectStrategy(RecursiveStrategy):
             else:
                 val_data = None
 
-            for model_i, horizon in enumerate(range(1, self.horizon // self.model_horizon + 1)):
+            for model_i, horizon in enumerate(range(1, intrinsic_horizon // self.model_horizon + 1)):
                 target_idx = index_slicer.create_idx_target(
                     dataset.data,
-                    self.horizon,
+                    intrinsic_horizon,
                     self.history,
                     self.step,
                     date_column=dataset.date_column,
@@ -133,7 +136,7 @@ class DirectStrategy(RecursiveStrategy):
                 if val_dataset:
                     val_target_idx = index_slicer.create_idx_target(
                         val_dataset.data,
-                        self.horizon,
+                        intrinsic_horizon,
                         self.history,
                         self.step,
                         date_column=val_dataset.date_column,
@@ -166,7 +169,7 @@ class DirectStrategy(RecursiveStrategy):
                 self.trainers.append(current_trainer)
 
         else:
-            for model_i, horizon in enumerate(range(1, self.horizon // self.model_horizon + 1)):
+            for model_i, horizon in enumerate(range(1, intrinsic_horizon // self.model_horizon + 1)):
                 features_idx = index_slicer.create_idx_data(
                     dataset.data,
                     self.model_horizon * horizon,
@@ -248,21 +251,24 @@ class DirectStrategy(RecursiveStrategy):
         return self
 
     def make_step(
-        self, step: int, dataset: TSDataset, inverse_transform: bool = True
+        self, step: int, horizon: int, dataset: TSDataset, inverse_transform: bool = True
     ) -> TSDataset:
         """Make a step in the direct strategy.
 
         Args:
             step: the step number.
+            horizon: the horizon length.
             dataset: the dataset to make the step on.
 
         Returns:
             the updated dataset.
 
         """
+        assert horizon % self.model_horizon == 0
+    
         test_idx = index_slicer.create_idx_test(
             dataset.data,
-            self.horizon,
+            horizon,
             self.history,
             self.step,
             date_column=dataset.date_column,
@@ -270,7 +276,7 @@ class DirectStrategy(RecursiveStrategy):
         )
         target_idx = index_slicer.create_idx_target(
             dataset.data,
-            self.horizon,
+            horizon,
             self.history,
             self.step,
             date_column=dataset.date_column,
@@ -280,7 +286,7 @@ class DirectStrategy(RecursiveStrategy):
         data = self.pipeline.create_data_dict_for_pipeline(dataset, test_idx, target_idx)
         data = self.pipeline.transform(data)
 
-        pred = self.trainers[step].predict(data, self.pipeline)
+        pred = self.trainers[step % len(self.trainers)].predict(data, self.pipeline)
         if inverse_transform:
             pred = self.pipeline.inverse_transform_y(pred)
 
